8a6caf73debd11bd624cebdbeb5d6d9073aea4b3
Luan Ta <luan@tinypulse.com>
2017-06-17 17:48:26 +0700

    Done to Search funciton

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,22 @@ class HomeController < ApplicationController
 
   def menu
     @sections = Section.all
+
+    if params[:section].blank?
+      @food_items = FoodItem.all
+    else
+      @section = Section.where(name: params[:section]).first
+      @food_items = @section.food_items
+    end
+
+    if params[:sort_column]
+      @food_items = @food_items.order("#{params[:sort_column]} #{params[:sort_direction]}")
+    end
+
+    if params[:search].present?
+      @food_items = @food_items.where('lower(name) like ?', "%#{params[:search].downcase}%")
+    end
+
   end
 
   def contact_us
--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -2,7 +2,7 @@ class FoodItem < ApplicationRecord
   belongs_to :section
 
   def image_ur_or_default
-    defaul_image_url = 'http://committoinclusion.org/wp-content/uploads/2015/05/YOUR-LOGO-HERE-450x450.png'
+    defaul_image_url = "http://loremflickr.com/320/240/#{name.gsub(' ','+')}"
     image_url.present? ? image_url : defaul_image_url
   end
 
