bff8b9e5f61e847201edf82b86744d0ecfa4d824
Luan Ta <luan@tinypulse.com>
2017-06-17 12:27:30 +0700

    Done for fooditems display sceen

--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -1,3 +1,9 @@
 class FoodItem < ApplicationRecord
   belongs_to :section
+
+  def image_ur_or_default
+    defaul_image_url = 'http://committoinclusion.org/wp-content/uploads/2015/05/YOUR-LOGO-HERE-450x450.png'
+    image_url.present? ? image_url : defaul_image_url
+  end
+
 end
